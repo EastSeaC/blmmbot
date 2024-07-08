541b3232f5d168968a73cac1e0564393d40d0116
OrientSea <815208129@qq.com>
2024-07-08 19:48:59 +0800

    123

--- lib/ServerManager.py
+++ lib/ServerManager.py
@@ -4,15 +4,19 @@ import win32gui, win32ui, win32con, win32api
 class ServerManager:
     @staticmethod
     def RestartBLMMServer():
+        for hwnd, title in get_all_windows():
+            print(f'句柄: {hwnd}, 标题: {title}')
+        # 获取所有顶层窗口句柄和标题
 
-        #   GetDesktopWindow 获得代表整个屏幕的一个窗口（桌面窗口）句柄
-        hd = win32gui.GetDesktopWindow()
 
-        # 获取所有子窗口
-        hwndChildList = []
-        #   EnumChildWindows 为指定的父窗口枚举子窗口
-        win32gui.EnumChildWindows(hd, lambda hwnd, param: param.append(hwnd), hwndChildList)
-        print(hwndChildList)
-        for hwnd in hwndChildList:
-            #   GetWindowText 取得一个窗体的标题（caption）文字，或者一个控件的内容
-            print("句柄：", hwnd, "标题：", win32gui.GetWindowText(hwnd))
+def foreach_window(hwnd, windows):
+    # 获取窗口标题
+    title = win32gui.GetWindowText(hwnd)
+    # 添加窗口信息到列表
+    windows.append((hwnd, title))
+
+
+def get_all_windows():
+    windows = []
+    win32gui.EnumWindows(lambda hwnd, param: foreach_window(hwnd, windows), None)
+    return windows

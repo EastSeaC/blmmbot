a75e1a51d6eff6d13a1cfb7fd36c82a1e4de6d72
OrientSea <815208129@qq.com>
2024-12-10 11:43:14 +0800

    更改session调用凡是

--- botCommands/configBot.py
+++ botCommands/configBot.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from khl import Bot
 
 from LogHelper import LogHelper
 from init_db import get_session
 from kook.ChannelKit import EsChannels
 from tables import *
+from tables.ResetPlayer import DB_ResetPlayer
 
 session = get_session()
 g_channels: EsChannels
@@ -13,8 +16,25 @@ def init(bot: Bot, es_channels: EsChannels):
     global g_channels
     g_channels = es_channels
 
-    @bot.task.add_cron(month=12, day=10)
+    @bot.task.add_cron(minute=45)
     async def reset_all_player_data():
+        reset_record = session.query(DB_ResetPlayer).all()
+        if not reset_record:
+            return
+            # 获取今天的日期
+        today = datetime.today()
+        for i in reset_record:
+            reset_player: DB_ResetPlayer = i
+
+            if today.month == reset_player.month and today.day == reset_player.day and not reset_player.is_reset:
+                await reset_aux()
+                reset_player.is_reset = True
+
+                session.merge(reset_record)
+
+        session.commit()
+
+    async def reset_aux():
         some_records = session.query(DB_PlayerData).all()
 
         for i in some_records:
--- tables/ResetPlayer.py
+++ tables/ResetPlayer.py
@@ -5,9 +5,12 @@ from sqlalchemy import *
 from .base import Base
 
 
-class ResetPlayer(Base):
+class DB_ResetPlayer(Base):
     __tablename__ = "reset_player"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     timer = Column(DateTime, nullable=False)
     is_reset = Column(Boolean, nullable=False, default=False)
+
+    month = Column(Integer, nullable=False, default=1)
+    day = Column(Integer, nullable=False, default=1)

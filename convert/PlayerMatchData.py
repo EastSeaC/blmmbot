d326a0841a0405c2aaf18c2866d5ba6953404213
OrientSea <815208129@qq.com>
2024-12-11 00:27:21 +0800

    修复 字符串长度截取函数

--- convert/PlayerMatchData.py
+++ convert/PlayerMatchData.py
@@ -77,6 +77,7 @@ class TPlayerMatchData:
         return "".join(result)  # 返回截取后的字符串
 
     def is_spectator_by_score(self):
+        print(f'{self.player_name}  {self.kill is 0 and self.death is 0 and self.assist is 0 and self.damage is 0}')
         return self.kill is 0 and self.death is 0 and self.assist is 0 and self.damage is 0
 
     '''

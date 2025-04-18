f99f2feed26c20b145c89528b32dfea58477167e
OrientSea <815208129@qq.com>
2025-04-18 21:50:20 +0800

    调整选人频道指令

--- botCommands/matchBot.py
+++ botCommands/matchBot.py
@@ -80,7 +80,9 @@ def init(bot: Bot, es_channels: EsChannels):
 
                 sql_session = get_session()
                 today_midnight = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-                z = sql_session.execute(select(DB_WillMatchs).where(DB_WillMatchs.match_id_2 == match_id_2)).first()
+                z = sql_session.execute(select(DB_WillMatchs)
+                                        .order_by(desc(DB_WillMatchs.time_match))
+                                        .where(DB_WillMatchs.match_id_2 == match_id_2)).first()
 
                 # print(z)
                 if z is None or len(z) == 0:

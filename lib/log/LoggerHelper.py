ddcc79096da27a38384298713f1530f979f789ef
OrientSea <815208129@qq.com>
2025-04-23 09:52:58 +0800

    调整选人频道指令

--- lib/log/LoggerHelper.py
+++ lib/log/LoggerHelper.py
@@ -8,13 +8,16 @@ logger.setLevel(logging.INFO)
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
-# 每天午夜生成一个新日志文件，并保留最近 7 天的日志
+# 每天午夜生成一个新日志文件，并保留最近7天的日志
 log_file = os.path.join(log_dir, "app.log")
+
+# 创建带有UTF-8编码的TimedRotatingFileHandler
 handler = TimedRotatingFileHandler(
     log_file,
     when="midnight",  # 每天午夜
     interval=1,
-    backupCount=7
+    backupCount=7,
+    encoding='utf8'  # 使用UTF-8编码
 )
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -22,4 +25,4 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
-logger.info("初始日志消息")  # 每天会生成一个新的日志文件，如 app.log.2024-01-01
+logger.info("初始日志消息 - 这是一个UTF-8测试: 😊✔️你好世界")  # 包含emoji和中文字符

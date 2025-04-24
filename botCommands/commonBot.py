3bb0345a2237688a680ee0e04b49d1b9dfae21f0
OrientSea <815208129@qq.com>
2025-04-24 15:53:54 +0800

    调整选人频道指令

--- botCommands/commonBot.py
+++ botCommands/commonBot.py
@@ -490,7 +490,7 @@ def init(bot: Bot, es_channels: EsChannels):
             try:
                 await es_channels.command_channel.send(cm)
                 for i in cm:
-                    logger.info(repr(i))
+                    logger.info(json.dumps(i))
             except Exception as e:
                 logger.exception("发生异常:")
                 logger.debug('*' * 30)
